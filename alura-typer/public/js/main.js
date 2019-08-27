4eff3c3091b47fe7e4bf1ab072d9684f0f5b7ec2
musashisama <48837659+musashisama@users.noreply.github.com>
2019-08-27 15:16:10 -0300

    Final aula 5

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -5,6 +5,7 @@ $(function(){
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
 });
 
@@ -42,20 +43,35 @@ function inicializaCronometro() {
             if (tempoRestante < 1) {
                 campo.attr("disabled", true);
                 clearInterval(cronometroID);
+                campo.toggleClass("campo-desativado");
                 $("#botao-reiniciar").attr("disabled", false);
             }
         }, 1000);
     });
 }
 
-$("#botao-reiniciar").click(reiniciaJogo);
-
 function reiniciaJogo(){
     campo.attr("disabled", false);
     campo.val("");
+    campo.toggleClass("campo-desativado");
     $("#contador-palavras").text("0");
-    $("#contador-caracteres").text("0");
-
+    $("#contador-caracteres").text("0");    
     $("#tempo-digitacao").text(tempoInicial);
     inicializaCronometro();
+}
+
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0 , digitado.length);
+
+        if(digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
 }
